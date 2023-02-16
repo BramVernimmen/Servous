8c49a3847c7fe2da3386f72c3ed7f5b9813388ee
Melodytje <hanne@soubry.net>
2023-02-16 11:21:25 +0100

    Add killplane for drinks
    Added killplane to destroy drinks that fell on the ground

--- GameJamGame/Assets/Scripts/Bottle.cs
+++ GameJamGame/Assets/Scripts/Bottle.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class Bottle : MonoBehaviour
 {
     const string m_Tag = "Bottles";
+    bool destroyed = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if(!other.gameObject.CompareTag(m_Tag))
         {
-            Debug.Log("Destroy bottle");
+            if(!destroyed)
+            {
+                Debug.Log("Destroy bottle");
+                BottleSpawner.Instance.DestroyedBottle(gameObject);
+                destroyed = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void KillBottle()
+    {
+        if (!destroyed)
+        {
+            Debug.Log("Destroy bottle on ground");
             BottleSpawner.Instance.DestroyedBottle(gameObject);
+            destroyed = true;
             Destroy(gameObject);
         }
     }
